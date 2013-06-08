356acee45ba7f57982b3e8d2bf362b4ab3b815b4
Dale Ross <kingdale16@hotmail.com>
2013-06-08 13:40:50 -0400

    Fixed buh with hiding links

--- manageleague.php
+++ manageleague.php
@@ -162,7 +162,7 @@ function processWorksheetStandings($key,$handicap)
 
 	foreach($feed->entries as $entry) {
 		$worksheetname = $entry->title->text;
-		if((strpos(strtolower($worksheetname), 'teams')!==false) ||(strpos(strtolower($worksheetname), 'links'))
+		if((strpos(strtolower($worksheetname), 'teams')!==false) ||(strpos(strtolower($worksheetname), 'links')!==false))
  			continue;
 		$currWkshtId = explode('/', $entry->id->text);
 		$currWkshtId = $currWkshtId[8];
