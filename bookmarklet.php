c7493ab28d76b0ee5d1d8c04e1639cb2bbe2d478
Dale Ross <kingdale16@hotmail.com>
2013-06-11 18:56:30 -0400

    Updated bookmarklet page with secure links

--- bookmarklet.php
+++ bookmarklet.php
@@ -7,9 +7,9 @@
 </head>
 <body>
 <div style="width:640px;text-align:center;margin-left:auto;margin-right:auto">
-<a href="javascript:function%20loadR4Script(url,%20callback)%20{%20var%20script%20=%20document.createElement(&quot;script&quot;);%20script.type%20=%20&quot;text/javascript&quot;;%20if(script.readyState)%20{%20%20script.onreadystatechange%20=%20function()%20{%20%20%20if%20(script.readyState%20==%20&quot;loaded&quot;%20%20%20%20%20||%20script.readyState%20==%20&quot;complete&quot;)%20{%20%20script.onreadystatechange%20=%20null;%20callback();%20%20%20}%20%20};%20}%20else%20{%20script.onload%20=%20function()%20{%20%20%20callback();%20%20};%20}%20script.src%20=%20url;%20document.getElementsByTagName(&quot;head&quot;)[0].appendChild(script);}if%20(typeof(runTileTracker)%20==%20&quot;undefined&quot;)%20{loadR4Script(&quot;http://www.dalevross.com/scrabtourneyasst/js/lexwstiletracker.js&quot;,function()%20{runTileTracker();});}%20else{%20runTileTracker();}"
+<a href="javascript:function%20loadR4Script(url,%20callback)%20{%20var%20script%20=%20document.createElement(&quot;script&quot;);%20script.type%20=%20&quot;text/javascript&quot;;%20if(script.readyState)%20{%20%20script.onreadystatechange%20=%20function()%20{%20%20%20if%20(script.readyState%20==%20&quot;loaded&quot;%20%20%20%20%20||%20script.readyState%20==%20&quot;complete&quot;)%20{%20%20script.onreadystatechange%20=%20null;%20callback();%20%20%20}%20%20};%20}%20else%20{%20script.onload%20=%20function()%20{%20%20%20callback();%20%20};%20}%20script.src%20=%20url;%20document.getElementsByTagName(&quot;head&quot;)[0].appendChild(script);}if%20(typeof(runTileTracker)%20==%20&quot;undefined&quot;)%20{loadR4Script(&quot;https://scrabtourneyasst.herokuapp.com/scrabtourneyasst/js/lexwstiletracker-s.js&quot;,function()%20{runTileTracker();});}%20else{%20runTileTracker();}"
 title="Drag me to your Bookmarks /Favourites Toolbar and click me to check your remaining tiles in a game">
-<img border="0" alt="Lex/WS Tile Tracker v1" src="http://www.dalevross.com/scrabtourneyasst/tourneyasst75x75.png" />
+<img border="0" alt="Lex/WS Tile Tracker v2" src="https://scrabtourneyasst.herokuapp.com/scrabtourneyasst/tourneyasst75x75.png" />
 </a>
 <br/>
 <span>Drag the image above to your bookmarks/favorites toolbar<br/>and click it during a game to get tile counts</span>
