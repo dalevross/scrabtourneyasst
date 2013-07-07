f6436c0897662c7e1ba2d7bd62bd1b89b5906956
Dale Ross <kingdale16@hotmail.com>
2013-07-07 00:56:49 -0400

    recommit Elliot's changes

--- lexwstracker/popup.js
+++ lexwstracker/popup.js
@@ -10,6 +10,8 @@ var trackingGenerator = {
 		$dialog = $('#lexwstracker');
 		
 		var dist = {"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2};
+		var vowels = "AEIOU";
+		var vcnt=0;var ccnt=0;var bcnt=0;
 		var left = {};
 		//var applink =  chrome.extension.getBackgroundPage().currentUrl;
 		var game = applink.match(/(lexulous|wordscraper|ea_scrabble_closed|livescrabble)/g);
@@ -43,11 +45,15 @@ var trackingGenerator = {
 				     var inbag = (tilecount>7)?tilecount-7:0;
 				     html = html + '<span style="font-weight:bold;">Tile Count: ' + tilecount + '</span><span> ('+ inbag + ' in bag)</span><br/>';
 						for (var letter in left) {
+							if(vowels.indexOf(letter)>-1) {vcnt=vcnt+left[letter]}
+							else {
+								if(letter!="blank") {ccnt=ccnt+left[letter]}else{bcnt=left[letter]}
+							}
 							if((index % 8)===0)
 							{
 								html = html + '<div style="float:left;padding:10px">';
 							}
-							html = html + '<span style="color:' + color + '" title="Total: ' + dist[letter] +  '">' + letter + ' - ' + left[letter] + '</span><br/>';
+							html = html + '<div class="wrapper"><div   class="letter" title="Total: ' + dist[letter] +  '">' + letter + '</div><div class="count">' + left[letter] + '</div></div><br/>';
 							index++;
 							if(((index)%8===0 && index!==0)|| index===Object.keys(left).length )
 							{
@@ -61,17 +67,15 @@ var trackingGenerator = {
 					
 					
 
-					html = html + '<div style="clear:both"/>';
+					html = html + '<div style="clear:both"/><br/><span class="vccnt">Vowels: '+vcnt+', Consonants: '+ccnt+', Blanks: '+bcnt+'.</span>';
 
 					//html = html + '<span id="trackerstat">' . $status . '</span><br/><br/>';
 
 
-					html = html + '<span>Brought to you by<br/><a href="http://www.facebook.com/lexandws?ref=ts" target="_blank" ><span style="text-decoration:underline;color:blue;">Lexulous/Wordscraper Tournaments</span></a></span>';
 					
-					html = html + '<br/><br/><span>Contact <a href="http://www.facebook.com/dvross" target="_blank" ><span style="text-decoration:underline;color:blue;">Dale V. Ross</span></a> for support or suggestions</span>';
 					var d = new Date();
 
-					var suffix = '<br/><span> Retrieved at ' + d.toLocaleString() + '</span>';
+					var suffix = '<br/><span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
 
 					html = html + suffix;
 					
