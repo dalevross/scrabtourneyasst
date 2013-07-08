7f156a0c8eeabdc03e1cf4ab16da89b925f18b15
Dale Ross <kingdale16@hotmail.com>
2013-07-08 08:54:15 -0400

    Attempting to use multiple dictionaries

--- lexwstracker/eaparser.js
+++ lexwstracker/eaparser.js
@@ -5,24 +5,45 @@ if(/http(s)?:\/\/scrabblefb-live2\.sn\.eamobile\.com\/live\/http(s)?\//.test(win
 		  
 	    if (request.command == "sendresults")
 	    {
-	    	  var used  = {"A":0,"B":0,"C":0,"D":0,"E":0,"F":0,"G":0,"H":0,"I":0,"J":0,"K":0,"L":0,"M":0,"N":0,"O":0,"P":0,"Q":0,"R":0,"S":0,"T":0,"U":0,"V":0,"W":0,"X":0,"Y":0,"Z":0,"blank":-26 };
-	    	  $("div.tile.inactive").each(function (i) {
-	    	        if ($(this).find("span.score_0").length > 0) {
-	    	        	used["blank"]++;
+	    	  var used  = {};
+	    	  var dist = {};
+	    	  
+	    	  //var longdict =;
+	    	  
+	    	  $("div.tileTrackingItem").each(function (i) {
+	    		    var tileletter = $.trim($(this).find("span.tileTrackingLetter").eq(0).text());
+	    		    var tilecount = $.trim($(this).find("span.tileTrackingCount").eq(0).text());
+	    	        if (tileletter==="") {
+	    	        	used["blank"]=0;
+	    	        	dist["blank"]=parseInt(tilecount);
 	    	        } else {
-	    	        	used[$(this).data("letter")]++;
+	    	        	used[tileletter]=0;
+	    	        	dist[tileletter]=parseInt(tilecount);
 	    	        }
 	    	      });
 	    	  
+	    	  $("div.tile.inactive").each(function (i) {
+	    		  if($(this).data("id")!="-1")
+	    		  {
+		    	        if ($(this).find("span.score_0").length > 0) {
+		    	        	used["blank"]++;
+		    	        } else {
+		    	        	used[$(this).data("letter")]++;
+		    	        }
+		    	        
+		    	        
+	    		  }
+	    	      });
+	    	  
 	    	  $("div.tile.active").each(function (i) {
-	    	        if ($(this).find("span.score_0").length > 0) {
+	    		  if (($(this).find("span.score_0").length > 0)&&($(this).data("id")!="-1")) {
 	    	        	used["blank"]++;
 	    	        } else {
 	    	        	used[$(this).data("letter")]++;
 	    	        }
 	    	      });
 	       //var resptext = $("p.gameCardScore").first().text();
-	       sendResponse({data: used});
+	       sendResponse({used: used,dist: dist});
 	    }
   });
 }
--- lexwstracker/popup.js
+++ lexwstracker/popup.js
@@ -1,6 +1,56 @@
 
 
 var trackingGenerator = {
+		
+	  loadToDialog: function($dialog,dist,used,game){
+		  var left = {};
+		  var tilecount = 0;
+		  var vowels = "AEIOU";
+		  var vcnt=0;var ccnt=0;var bcnt=0;
+			
+		     for(var letter in used){
+		    	    if((dist[letter]-used[letter])>0)
+		    	    {
+		    	    	left[letter] = dist[letter]-used[letter];
+		    	    	tilecount+=dist[letter]-used[letter];
+		    	    }
+		    	}
+		     //var color = (game==="lexulous")?"#2BB0E8":"red";
+		     var index = 0;
+		     var html = '';
+		     var inbag = (tilecount>7)?tilecount-7:0;
+		     html = html + '<span style="font-weight:bold;">Tile Count: ' + tilecount + '</span><span> ('+ inbag + ' in bag)</span><br/>';
+				for (var letter in left) {
+					if(vowels.indexOf(letter)>-1) {vcnt=vcnt+left[letter];}
+					else {
+						if(letter!="blank") {ccnt=ccnt+left[letter];}else{bcnt=left[letter];}
+					}
+					if((index % 8)===0)
+					{
+						html = html + '<div style="float:left;padding:10px">';
+					}
+					html = html + '<div class="wrapper"><div   class="letter" title="Total: ' + dist[letter] +  '">' + letter + '</div><div class="count">' + left[letter] + '</div></div><br/>';
+					index++;
+					if(((index)%8===0 && index!==0)|| index===Object.keys(left).length )
+					{
+						html = html + '</div>';
+					}
+				}
+				
+				
+			
+
+			html = html + '<div style="clear:both"/><br/><span class="vccnt">Vowels: '+vcnt+', Consonants: '+ccnt+', Blanks: '+bcnt+'.</span>';
+	
+			var d = new Date();
+
+			var suffix = '<br/><span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
+
+			html = html + suffix;
+			
+			$dialog.html(html);
+		  
+	  },
 	  getTilesLeft: function(applink) {
 
 		var html = '';
@@ -10,10 +60,8 @@ var trackingGenerator = {
 		$dialog = $('#lexwstracker');
 		
 		var dist = {"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2};
-		var vowels = "AEIOU";
-		var vcnt=0;var ccnt=0;var bcnt=0;
-		var left = {};
-		//var applink =  chrome.extension.getBackgroundPage().currentUrl;
+		
+		// var applink = chrome.extension.getBackgroundPage().currentUrl;
 		var game = applink.match(/(lexulous|wordscraper|ea_scrabble_closed|livescrabble)/g);
 
 		if (game === null) {
@@ -30,56 +78,9 @@ var trackingGenerator = {
 			$dialog.html(loadinghtml);
 			 chrome.tabs.query({'active': true}, function (tabs) {
 				 chrome.tabs.sendMessage(tabs[0].id, {command: "sendresults"}, function(response) {
-				     var used = response.data;
-				     var tilecount = 0;
-				     for(var letter in used){
-				    	    if((dist[letter]-used[letter])>0)
-				    	    {
-				    	    	left[letter] = dist[letter]-used[letter];
-				    	    	tilecount+=dist[letter]-used[letter];
-				    	    }
-				    	}
-				     var color = (game==="lexulous")?"#2BB0E8":"red";
-				     var index = 0;
-				     var html = '';
-				     var inbag = (tilecount>7)?tilecount-7:0;
-				     html = html + '<span style="font-weight:bold;">Tile Count: ' + tilecount + '</span><span> ('+ inbag + ' in bag)</span><br/>';
-						for (var letter in left) {
-							if(vowels.indexOf(letter)>-1) {vcnt=vcnt+left[letter];}
-							else {
-								if(letter!="blank") {ccnt=ccnt+left[letter];}else{bcnt=left[letter];}
-							}
-							if((index % 8)===0)
-							{
-								html = html + '<div style="float:left;padding:10px">';
-							}
-							html = html + '<div class="wrapper"><div   class="letter" title="Total: ' + dist[letter] +  '">' + letter + '</div><div class="count">' + left[letter] + '</div></div><br/>';
-							index++;
-							if(((index)%8===0 && index!==0)|| index===Object.keys(left).length )
-							{
-								html = html + '</div>';
-							}
-						}
-						
-					//}
-					
-					//$html = $html . 'The tile tracker is currently down for maintenance. <br/>';
-					
-					
-
-					html = html + '<div style="clear:both"/><br/><span class="vccnt">Vowels: '+vcnt+', Consonants: '+ccnt+', Blanks: '+bcnt+'.</span>';
-
-					//html = html + '<span id="trackerstat">' . $status . '</span><br/><br/>';
-
-
-					
-					var d = new Date();
-
-					var suffix = '<br/><span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
-
-					html = html + suffix;
-					
-					 $dialog.html(html);
+				     var used = response.used;
+				     var dist = response.dist;
+				     trackingGenerator.loadToDialog($dialog,dist,used,game);
 					  });
 				});
 		
