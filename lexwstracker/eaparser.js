a638561e66bcc30bc6a8817ed0555d9b76131af5
dross <kingdale16@hotmail.com>
2013-07-10 08:45:49 -0400

    removed error from managetbreak
    Merged Elliot's changes to version 1.9.0

--- lexwstracker/eaparser.js
+++ lexwstracker/eaparser.js
@@ -1,107 +1,137 @@
 if(/http(s)?:\/\/scrabblefb-live2\.sn\.eamobile\.com\/live\/http(s)?\//.test(window.location.href)){
-	
+
 	chrome.runtime.onMessage.addListener(
-	  function(request, sender, sendResponse) {
-		  
-	    if (request.command == "sendresults")
-	    {
-	    	
-	    	if($("p.gameCardName").eq(0).text()=="You ") {
-				var playerScore = $("p.gameCardScore").eq(0).text();
-				var oppoScore = $("p.gameCardScore").eq(1).text(); 			}
-			else {
-				var playerScore = $("p.gameCardScore").eq(1).text();
-				var oppoScore = $("p.gameCardScore").eq(0).text(); 		
-			}
-	    	
-	    	if($("p.gameCardName").eq(0).text()=="You ") {
-				var oppoName = $.trim($("p.gameCardName").eq(1).text());
-				var oppoID=$('img[class="cardAvatar"]').eq(1).attr('src').split('_')[1];
-			}
-			else {
-				var oppoName = $.trim($("p.gameCardName").eq(0).text());
-				var oppoID=$('img[class="cardAvatar"]').eq(0).attr('src').split('_')[1];
-			}
-	    	
-			var playerID=localStorage.cacheUser;
-			
-			
-			var finished;
-			
-			if($("div#eog1Place img").length == 0)
-			{
-				finished = false;
-			}
-			else
-			{
-				var winnerId = $("div#eog1Place img").attr('src').split('_')[1];
-				var winnerScore = $("div#eog1Place div.eogTotal div.value").text();
-				var loserId = $("div#eog2Place img").attr('src').split('_')[1];
-				var loserScore = $("div#eog2Place div.eogTotal div.value").text();
-				
-				if(winnerId===playerID)
+			function(request, sender, sendResponse) {
+
+				if (request.command == "sendresults")
 				{
-					finished = ((winnerScore == playerScore) && (loserId == oppoID) && (oppoScore==loserScore))
+
+					if($("p.gameCardName").eq(0).text()=="You ") {
+						var playerScore = $("p.gameCardScore").eq(0).text();
+						var oppoScore = $("p.gameCardScore").eq(1).text(); 	
+					}
+					else {
+						var playerScore = $("p.gameCardScore").eq(1).text();
+						var oppoScore = $("p.gameCardScore").eq(0).text(); 		
+					}
+
+					if($("p.gameCardName").eq(0).text()=="You ") {
+						var oppoName = $.trim($("p.gameCardName").eq(1).text());
+						var oppoID=$('img[class="cardAvatar"]').eq(1).attr('src').split('_')[1];
+					}
+					else {
+						var oppoName = $.trim($("p.gameCardName").eq(0).text());
+						var oppoID=$('img[class="cardAvatar"]').eq(0).attr('src').split('_')[1];
+					}
+
+					var playerID=localStorage.cacheUser;
+
+
+					var finished;
+
+					if($("div#eog1Place img").length == 0)
+					{
+						finished = false;
+					}
+					else
+					{
+						var winnerId = $("div#eog1Place img").attr('src').split('_')[1];
+						var winnerScore = $("div#eog1Place div.eogTotal div.value").text();
+						var loserId = $("div#eog2Place img").attr('src').split('_')[1];
+						var loserScore = $("div#eog2Place div.eogTotal div.value").text();
+
+						if(winnerId===playerID)
+						{
+							finished = ((winnerScore == playerScore) && (loserId == oppoID) && (oppoScore==loserScore))
+						}
+						else if(winnerId===oppoID)
+						{
+							finished = ((winnerScore == oppoScore) && (loserId == oppoScore) && (oppoScore==playerScore))
+						}		
+
+					}
+
+
+					var word=$('tr[class="even"]').eq(0).find('td').eq(1).find('p').text().split('\"')[1];
+					var word2=$('tr[class="even"]').eq(1).find('td').eq(1).find('p').text().split('\"')[1];
+
+					var used  = {};
+
+
+					var longdict = $.trim($("span#wordsBtnImage").attr('title'));
+					var shortdict = longdict.match(/(Collins|TWL|OSPD4|German|Spanish|Italian|French|Portuguese)/g);
+					
+					var langs={
+							"Collins":"en",
+							"TWL" :"en",
+							"OSPD4" : "en",
+							"French": "fr",
+							"Italian":"it",
+							"German": "de",
+							"Portuguese":"pt",
+							"Spanish": "es"
+					};
+					var lang=langs[shortdict];
+
+					var validWord="";
+					if($("p#lookupResult").find("span").attr("class")=="typo_green") {
+						validWord=$("p#lookupResult").find("span").text();
+						if(validWord.indexOf('Define')==-1) {
+							iconURL =  chrome.extension.getURL("owl-lookup.png");
+							$("p#lookupResult").find("span").html('<A href="http://google.com/search?hl='+lang+'&q=define:'+validWord+'" target="define"><img src="'+iconURL+'" align="left"> '+validWord+'</a>');
+						}
+					}
+					else {
+						if($("p#lookupResult").find("span").attr("class")=="typo_lightRed") {
+							// do nothing
+						}
+						else {
+							$("p#lookupResult").html('<span class="typo_lightRed">Click the owl for definitions of valid words</span>');
+						}
+					}
+
+					var alldist = {
+							"Collins":{"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2},
+							"TWL" :{"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2},
+							"OSPD4" :{"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2},
+							"French":{"A":9,"B":2,"C":2,"D":3,"E":15,"F":2,"G":2,"H":2,"I":8,"J":1,"K":1,"L":5,"M":3,"N":6,"O":6,"P":2,"Q":1,"R":6,"S":6,"T":6,"U":6,"V":2,"W":1,"X":1,"Y":1,"Z":1,"blank":2},
+							"Italian":{"A":14,"B":3,"C":6,"D":3,"E":11,"F":3,"G":2,"H":2,"I":12,"L":5,"M":5,"N":5,"O":15,"P":3,"Q":1,"R":6,"S":6,"T":6,"U":5,"V":3,"Z":2,"blank":2},
+							"German": {"A":5,"B":2,"C":2,"D":4,"E":15,"F":2,"G":3,"H":4,"I":6,"J":1,"K":2,"L":3,"M":4,"N":9,"O":3,"P":1,"Q":1,"R":6,"S":7,"T":6,"U":6,"V":1,"W":1,"X":1,"Y":1,"Z":1,"Ä":1,"Ö":1,"Ü":1,"blank":2},
+							"Portuguese":{"A":14,"B":3,"C":4,"D":5,"E":11,"F":2,"G":2,"H":2,"I":10,"J":2,"L":5,"M":6,"N":4,"O":10,"P":4,"Q":1,"R":6,"S":8,"T":5,"U":7,"V":2,"X":1,"Z":1,"Ç":2,"blank":3},
+							"Spanish": {"A":12,"B":2,"C":4,"D":5,"E":12,"F":1,"G":2,"H":2,"I":6,"J":1,"L":4,"M":2,"N":5,"O":9,"P":2,"Q":1,"R":5,"S":6,"T":4,"U":5,"V":1,"X":1,"Y":1,"Z":1,"CH":1,"LL":1,"Ñ":1,"RR":1,"blank":2}
+					};
+
+					var dist = alldist[shortdict];
+
+
+					$.each( dist, function( key, value ) {
+						used[key]=0;
+					});
+
+
+
+					$("div.tile.inactive").each(function (i) {
+						if($(this).data("id")!="-1")
+						{
+							if ($(this).find("span.score_0").length > 0) {
+								used["blank"]++;
+							} else {
+								used[$(this).data("letter")]++;
+							}
+
+
+						}
+					});
+
+					$("div.tile.active").each(function (i) {
+						if (($(this).find("span.score_0").length > 0)&&($(this).data("id")!="-1")) {
+							used["blank"]++;
+						} else {
+							used[$(this).data("letter")]++;
+						}
+					});
+					//var resptext = $("p.gameCardScore").first().text();
+					sendResponse({used: used,dist: dist,dictionary:shortdict,name: oppoName, ID:oppoID, first:word, second:word2, player:playerID, scoreP:playerScore, scoreO:oppoScore, finished: finished, defineMe:validWord});
 				}
-				else if(winnerId===oppoID)
-				{
-					finished = ((winnerScore == oppoScore) && (loserId == oppoScore) && (oppoScore==playerScore))
-				}		
-				
-			}
-			
-
-			var word=$('tr[class="even"]').eq(0).find('td').eq(1).find('p').text().split('\"')[1];
-			var word2=$('tr[class="even"]').eq(1).find('td').eq(1).find('p').text().split('\"')[1];
-
-	    	  var used  = {};
-
-	    	  
-	    	  var longdict = $.trim($("span#wordsBtnImage").attr('title'));
-	    	  var shortdict = longdict.match(/(Collins|TWL|OSPD4|German|Spanish|Italian|French|Portuguese)/g);
-	    	  
-	    	  var alldist = {
-	    			  "Collins":{"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2},
-	    			  "TWL" :{"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2},
-	    			  "OSPD4" :{"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2},
-	    			  "French":{"A":9,"B":2,"C":2,"D":3,"E":15,"F":2,"G":2,"H":2,"I":8,"J":1,"K":1,"L":5,"M":3,"N":6,"O":6,"P":2,"Q":1,"R":6,"S":6,"T":6,"U":6,"V":2,"W":1,"X":1,"Y":1,"Z":1,"blank":2},
-	    			  "Italian":{"A":14,"B":3,"C":6,"D":3,"E":11,"F":3,"G":2,"H":2,"I":12,"L":5,"M":5,"N":5,"O":15,"P":3,"Q":1,"R":6,"S":6,"T":6,"U":5,"V":3,"Z":2,"blank":2},
-	    			  "German": {"A":5,"B":2,"C":2,"D":4,"E":15,"F":2,"G":3,"H":4,"I":6,"J":1,"K":2,"L":3,"M":4,"N":9,"O":3,"P":1,"Q":1,"R":6,"S":7,"T":6,"U":6,"V":1,"W":1,"X":1,"Y":1,"Z":1,"Ä":1,"Ö":1,"Ü":1,"blank":2},
-	    			  "Portuguese":{"A":14,"B":3,"C":4,"D":5,"E":11,"F":2,"G":2,"H":2,"I":10,"J":2,"L":5,"M":6,"N":4,"O":10,"P":4,"Q":1,"R":6,"S":8,"T":5,"U":7,"V":2,"X":1,"Z":1,"Ç":2,"blank":3},
-	    			  "Spanish": {"A":12,"B":2,"C":4,"D":5,"E":12,"F":1,"G":2,"H":2,"I":6,"J":1,"L":4,"M":2,"N":5,"O":9,"P":2,"Q":1,"R":5,"S":6,"T":4,"U":5,"V":1,"X":1,"Y":1,"Z":1,"CH":1,"LL":1,"Ñ":1,"RR":1,"blank":2}
-	    			  };
-	    	  
-	    	  var dist = alldist[shortdict];
-	    	  
-	    	  
-	    	  $.each( dist, function( key, value ) {
-	    		  		used[key]=0;
-	    		  });
-	    	  
-	    	  
-	    	  
-	    	  $("div.tile.inactive").each(function (i) {
-	    		  if($(this).data("id")!="-1")
-	    		  {
-		    	        if ($(this).find("span.score_0").length > 0) {
-		    	        	used["blank"]++;
-		    	        } else {
-		    	        	used[$(this).data("letter")]++;
-		    	        }
-		    	        
-		    	        
-	    		  }
-	    	      });
-	    	  
-	    	  $("div.tile.active").each(function (i) {
-	    		  if (($(this).find("span.score_0").length > 0)&&($(this).data("id")!="-1")) {
-	    	        	used["blank"]++;
-	    	        } else {
-	    	        	used[$(this).data("letter")]++;
-	    	        }
-	    	      });
-	       //var resptext = $("p.gameCardScore").first().text();
-	       sendResponse({used: used,dist: dist,dictionary:shortdict,name: oppoName, ID:oppoID, first:word, second:word2, player:playerID, scoreP:playerScore, scoreO:oppoScore, finished: finished});
-	    }
-  });
+			});
 }
--- lexwstracker/popup.js
+++ lexwstracker/popup.js
@@ -1,69 +1,80 @@
 
 
 var trackingGenerator = {
-		
-	  loadToDialog: function($dialog,response,game){
-		  
-		  var used = response.used;
-		  var dist = response.dist;
-		  var oppoName = response.name;
-		  var oppoID= response.ID;
-		  var word=response.first;
-		  var dictionary=response.dictionary;
-		  var playerId=response.player;
-		  var playerScore=response.scoreP;
-		  var oppoScore=response.scoreO;
-		  var finished = response.finished;
-		  
-		  var left = {};
-		  var tilecount = 0;
-		  var vowels = "AEIOU";
-		  var vcnt=0;var ccnt=0;var bcnt=0;
-		  
-		  if(!word) {word=response.second;}
 
-		  for(var letter in used){
-			  if((dist[letter]-used[letter])>0)
-		    	    {
-		    	    	left[letter] = dist[letter]-used[letter];
-		    	    	tilecount+=dist[letter]-used[letter];
-		    	    }
-		    	}
-		     //var color = (game==="lexulous")?"#2BB0E8":"red";
-		     var index = 0;
-		     var html = '';
-		     html=html+'Your game with '+oppoName+' (<a href="http://facebook.com/'+oppoID+'" target="_blank">View Profile</a>)<br/><br/>';
-				
-		     var inbag = (tilecount>7)?tilecount-7:0;
-		     html = html + '<span style="font-weight:bold;">Tile Count: ' + tilecount + '</span><span> ('+ inbag + ' in bag)</span><br/>';
-				for (var letter in left) {
-					if(vowels.indexOf(letter)>-1) {vcnt=vcnt+left[letter];}
-					else {
-						if(letter!="blank") {ccnt=ccnt+left[letter];}else{bcnt=left[letter];}
-					}
-					if((index % 8)===0)
-					{
-						html = html + '<div style="float:left;padding:10px">';
-					}
-					html = html + '<div class="wrapper"><div   class="letter" title="Total: ' + dist[letter] +  '">' + letter + '</div><div class="count">' + left[letter] + '</div></div>';
-					index++;
-					if(((index)%8===0 && index!==0)|| index===Object.keys(left).length )
-					{
-						html = html + '</div>';
-					}
+		loadToDialog: function($dialog,response,game){
+
+			var used = response.used;
+			var dist = response.dist;
+			var oppoName = response.name;
+			var oppoID= response.ID;
+			var word=response.first;
+			var dictionary=response.dictionary;
+			var playerId=response.player;
+			var playerScore=response.scoreP;
+			var oppoScore=response.scoreO;
+			var finished = response.finished;
+			var toDefine = response.defineMe;
+
+			var left = {};
+			var tilecount = 0;
+			var vowels = "AEIOU";
+			var vcnt=0;var ccnt=0;var bcnt=0;
+
+			if(!word) {word=response.second;}
+
+			for(var letter in used){
+				if((dist[letter]-used[letter])>0)
+				{
+					left[letter] = dist[letter]-used[letter];
+					tilecount+=dist[letter]-used[letter];
 				}
-				
-				
+			}
 			
+			var index = 0;
+			var html = '';
 
-			html = html + '<div style="clear:both"/><br/><span class="vccnt">Vowels: '+vcnt+', Consonants: '+ccnt+', Blanks: '+bcnt+'.</span>';
-	
+			html=html+'<div class="heading">Your game with '+oppoName+' <span class="profile"><a href="http://facebook.com/'+oppoID+'" target="_blank">Facebook Profile</a></span></div>';
+
+			var inbag = (tilecount>7)?tilecount-7:0;
+			html = html + '<span style="font-weight:bold;">Tile Count: ' + tilecount + '</span><span> ('+ inbag + ' in bag)</span><br/>';
+			for (var letter in left) {
+				if(vowels.indexOf(letter)>-1) {vcnt=vcnt+left[letter];}
+				else {
+					if(letter!="blank") {ccnt=ccnt+left[letter];}else{bcnt=left[letter];}
+				}
+				if((index % 8)===0)
+				{
+					html = html + '<div style="float:left;padding:10px">';
+				}
+				html = html + '<div class="wrapper"><div   class="letter" title="Total: ' + dist[letter] +  '">' + letter + '</div><div class="count">' + left[letter] + '</div></div>';
+				index++;
+				if(((index)%8===0 && index!==0)|| index===Object.keys(left).length )
+				{
+					html = html + '</div>';
+				}
+			}
+
+
+
+
+			html = html + '<div style="clear:both"/><div class="vccnt">Vowels: '+vcnt+', Consonants: '+ccnt+', Blanks: '+bcnt+'.</div>';
+
 			var d = new Date();
 
-			var suffix = '<br/><span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
+			var suffix = '<span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
 
 			html = html + suffix;
-			
+
+			var lang="en";
+			if(toDefine) {
+				if(dictionary=="German") {lang="de";}
+				if(dictionary=="Spanish") {lang="es";}
+				if(dictionary=="Italian") {lang="it";}
+				if(dictionary=="French") {lang="fr";}
+				if(dictionary=="Portuguese") {lang="pt";}
+				//	html=html+'<div class="define"><A href="http://google.com/search?hl='+lang+'&q=define:'+toDefine+'" target="define">Define '+toDefine+'</a></div>';
+			}
 			if(playerId=='593170373'||playerId=='712117020') {
 				if(finished) {
 					html=html + '<div class="sendScore"><form action="http://moltengold.com/cgi-bin/scrabble/extn.pl" method="post" target="scoring"> <input name="playerScore" value="'+ playerScore +'" type="hidden" > <input name="oppoScore" value="'+ oppoScore +'" type="hidden" >  <input name="playerId" value="'+playerId+'" type="hidden"> <input name="oppoId" value="'+oppoID+'" type="hidden">  <input name="dictionary" value="'+dictionary+'" type="hidden"> <input name="word" value="'+ word +'" type="hidden" > <input name="app" value="Scrabble" type="hidden"> Save final scores in Facebook Scrabble League <input type="submit" value="Save"></form></div>';
@@ -72,121 +83,121 @@ var trackingGenerator = {
 
 					html=html + '<div class="sendWord"><form action="http://moltengold.com/cgi-bin/scrabble/extn.pl" method="post" target="scoring"> <input name="word" value="'+ word +'" type="hidden" > <input name="playerId" value="'+playerId+'" type="hidden"> <input name="oppoId" value="'+oppoID+'" type="hidden">  <input name="dictionary" value="'+dictionary+'" type="hidden"> <input name="app" value="Scrabble" type="hidden"> Record first word ('+word+') in Facebook Scrabble League <input type="submit" value="Send"></form></div>';
 				}
-				
+
 			}
 
 			$dialog.html(html);
-		  
-	  },
-	  getTilesLeft: function(applink) {
 
-		var html = '';
-		
-		var $dialog;
-				
-		$dialog = $('#lexwstracker');
-		
-		var dist = {"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2};
-		
-		// var applink = chrome.extension.getBackgroundPage().currentUrl;
-		var game = applink.match(/(lexulous|wordscraper|ea_scrabble_closed|livescrabble)/g);
+		},
+		getTilesLeft: function(applink) {
 
-		if (game === null) {
+			var html = '';
 
-			$dialog.html('Invalid link,' + applink +',found in address bar!');		
+			var $dialog;
 
-			return false;
+			$dialog = $('#lexwstracker');
 
-		}
-		var loadinghtml = '<div><span>Loading...</span><br/><img src="trackerloading.gif" /></div>';
-		
-		if((game[0]=="ea_scrabble_closed")||(game[0]=="livescrabble"))
-		{
-			$dialog.html(loadinghtml);
-			 chrome.tabs.query({'active': true, 'currentWindow':true}, function (tabs) {
-				 chrome.tabs.sendMessage(tabs[0].id, {command: "sendresults"}, function(response) {
-				     var used = response.used;
-				     var dist = response.dist;
-				     trackingGenerator.loadToDialog($dialog,response,game);
-					  });
-				});
-		
-		}
-		else
-		{
-			var gid = /gid=(\d+)/g.exec(applink);
-	
-			if (gid === null) {
-	
-				$dialog.html('Invalid game link!');		
-	
+			var dist = {"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3,"H":2,"I":9,"J":1,"K":1,"L":4,"M":2,"N":6,"O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4,"V":2,"W":2,"X":1,"Y":2,"Z":1,"blank":2};
+
+			// var applink = chrome.extension.getBackgroundPage().currentUrl;
+			var game = applink.match(/(lexulous|wordscraper|ea_scrabble_closed|livescrabble)/g);
+
+			if (game === null) {
+
+				$dialog.html('Invalid link,' + applink +',found in address bar!');		
+
 				return false;
-	
-			}
-	
-			var pid = /pid=(\d)/g.exec(applink);
-			var password = /password=(\w+)/g.exec(applink);
-	
-			if ((pid === null) || (password === null)) {
-	
-				params = {gid : gid[1],game : game[0],version : 2,extension:1};
-	
-			} else {
-	
-				params = {gid : gid[1],game : game[0],pid : pid[1],password : password[1],version : 2,extension:1};
-	
-			}
-	
-	
-	
-			
-	
-	
-			$dialog.html(loadinghtml);
-	
-	
-	
-			$.ajax({url : 'https://scrabtourneyasst.herokuapp.com/scrabtourneyasst/scrabtiletracker.php?callback=?',
-	
-						context : this,
-	
-						data : (params),
-	
-						dataType : "jsonp",
-	
-						success : function(data) {					
-	
-							var d = new Date();
-	
-							var suffix = '<br/><span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
 
-							html = data['html'] + suffix;
-	
-							$dialog.html(html);
-	
-						},
-	
-						failure : function(jqXHR, textStatus, errorThrown) {
-	
-							$dialog.html(textStatus);
-	
-							return false;
-	
-						}
-	
+			}
+			var loadinghtml = '<div><span>Loading...</span><br/><img src="trackerloading.gif" /></div>';
+
+			if((game[0]=="ea_scrabble_closed")||(game[0]=="livescrabble"))
+			{
+				$dialog.html(loadinghtml);
+				chrome.tabs.query({'active': true, 'currentWindow':true}, function (tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {command: "sendresults"}, function(response) {
+						var used = response.used;
+						var dist = response.dist;
+						trackingGenerator.loadToDialog($dialog,response,game);
 					});
-		}
-		return true;
+				});
 
-	}
+			}
+			else
+			{
+				var gid = /gid=(\d+)/g.exec(applink);
+
+				if (gid === null) {
+
+					$dialog.html('Invalid game link!');		
+
+					return false;
+
+				}
+
+				var pid = /pid=(\d)/g.exec(applink);
+				var password = /password=(\w+)/g.exec(applink);
+
+				if ((pid === null) || (password === null)) {
+
+					params = {gid : gid[1],game : game[0],version : 2,extension:1};
+
+				} else {
+
+					params = {gid : gid[1],game : game[0],pid : pid[1],password : password[1],version : 2,extension:1};
+
+				}
+
+
+
+
+
+
+				$dialog.html(loadinghtml);
+
+
+
+				$.ajax({url : 'https://scrabtourneyasst.herokuapp.com/scrabtourneyasst/scrabtiletracker.php?callback=?',
+
+					context : this,
+
+					data : (params),
+
+					dataType : "jsonp",
+
+					success : function(data) {					
+
+						var d = new Date();
+
+						var suffix = '<br/><span style="font-size:10px"> Retrieved at ' + d.toLocaleString() + '</span>';
+
+						html = data['html'] + suffix;
+
+						$dialog.html(html);
+
+					},
+
+					failure : function(jqXHR, textStatus, errorThrown) {
+
+						$dialog.html(textStatus);
+
+						return false;
+
+					}
+
+				});
+			}
+			return true;
+
+		}
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
+//Run our tile tracker script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.query({'active': true,'currentWindow':true}, function (tabs) {
-			var applink = tabs[0].url;
-			trackingGenerator.getTilesLeft(applink);
-		});
-		
-		
+	chrome.tabs.query({'active': true,'currentWindow':true}, function (tabs) {
+		var applink = tabs[0].url;
+		trackingGenerator.getTilesLeft(applink);
+	});
+
+
 });
